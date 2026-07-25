2815e99970c572f0e92eafcbae3ac7ece201c106
Sejal Rawat <indiancoder06@gmail.com>
2026-07-25 22:14:54 +0530

    feat: setup routing and project structure

---
+++ src/constants/siteConfig.js
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  shopName: "Khandelwal Furniture",
+  tagline: "Crafting Beautiful Homes",
+
+  phone: "+91XXXXXXXXXX",
+
+  whatsapp: "91XXXXXXXXXX",
+
+  address: "Dhamnod, Madhya Pradesh",
+
+  email: "info@khandelwalfurniture.in",
+
+  instagram: "",
+
+  facebook: "",
+};
